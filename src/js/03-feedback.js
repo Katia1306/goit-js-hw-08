6e2be4e9fb891179dcf2035efb7fc7531382dc80
Katia1306 <kateryna.zhylan@gmail.com>
2023-10-17 23:12:02 +0200

    upd0

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
 
+const selectors = {
+    form: document.querySelector("form"),
+    input: document.querySelector("input"),
+    textarea: document.querySelector("textarea")
+}
+const LS_KEY = "feedback-form-state";
+const info = [];
+
+selectors.form.addEventListener('input', handlerClick);
+
+function handlerClick(evt) {
+    { }
+    
+    localStorage.setItem(LS_KEY, JSON.stringify(info));
+}
