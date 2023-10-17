79af46290d7fe171e5321120c49b7eff2fc583c9
Katia1306 <kateryna.zhylan@gmail.com>
2023-10-17 22:25:24 +0200

    update3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,11 @@
 import Player from '@vimeo/player';
+
+import throttle from 'lodash.throttle';
+
+
 const iframe = document.querySelector('iframe');
 
-const player = new Player('handstick', {
+const player = new Player('iframe', {
     id: 19231868,
     width: 640
 });
@@ -11,28 +15,20 @@ player.on('play', function() {
 });
 
 const onPlay = function(data) {
+   
     duration: 61.857
-    percent: 0.049
-    seconds: 3.034
-}
+    percent: 1
+    seconds: 61.857
+
+};
 
 player.on('play', onPlay);
 
 
-player.setCurrentTime(30.456).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video s duration
-            break;
 
-        default:
-            // some other error occurred
-            break;
-    }
-});
-player.on('timeupdate', throttle(function(data) {
-    const currentTime = data.seconds;
-    localStorage.setItem('videoplayer-current-time', currentTime.toString());
-}, 1000));
+player.on('timeupdate', throttle(function (data) {
+    player.getCurrentTime().then(function (seconds) {
+        console.log(seconds);
+        localStorage.setItem("videoplayer-current-time", seconds);
+    })
+},  1000));
